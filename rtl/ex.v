08c21d0de980388d014c4e4b911c226570e173ae
DawningSoon <dawningsoon@qq.com>
2024-11-20 21:06:14 +0800

    完成跳转jal与bne

--- rtl/ex.v
+++ rtl/ex.v
@@ -28,7 +28,7 @@ wire[4:0] rs2;
 wire[6:0] func7;
 wire[31:0] imm_I= {{21{ins_i[31]}}, ins_i[30:20]};
 wire[31:0] imm_S= {{21{ins_i[31]}}, ins_i[30:25], ins_i[11:8], ins_i[7]};
-wire[31:0] imm_B= {{20{ins_i[31]}}, ins_i[7], ins_i[30:25], 1'b0};
+wire[31:0] imm_B= {{20{ins_i[31]}}, ins_i[7], ins_i[30:25], ins_i[11:8], 1'b0};
 wire[31:0] imm_U= {ins_i[31:12], 12'h0};
 wire[31:0] imm_J= {{12{ins_i[31]}}, ins_i[19:12], ins_i[20], ins_i[30:21], 1'b0};
 
@@ -40,6 +40,7 @@ assign func3  = ins_i[14:12];
 assign rs1 	  = ins_i[19:15];
 assign rs2 	  = ins_i[24:20];
 assign func7  = ins_i[31:25];
+
 
 // assign imm_I 
 // assign imm    = ins_i[31:20];
@@ -104,19 +105,22 @@ always @(*) begin
             rd_data_o = 32'b0;
             rd_addr_o = `x0;
             rd_wr_en  = 1'b0;
+
             case (func3)
                 `INST_BEQ: begin
                     if(rs1_data_i == rs2_data_i)begin
                         jump_addr_o = ins_addr_i + imm_B;
                         jump_en_o	= 1'b1;
-                        hold_flag_o = 1'b0;	
+                        hold_flag_o = 1'b0;
+                        	
                     end
                 end
                 `INST_BNE: begin
-                    if(rs1_data_i != rs1_data_i)begin
+                    if(rs1_data_i != rs2_data_i)begin
                         jump_addr_o = ins_addr_i + imm_B;
                         jump_en_o	= 1'b1;
                         hold_flag_o = 1'b0;
+                        
                     end
                 end
                 default: begin
--- sim/tb.v
+++ sim/tb.v
@@ -8,8 +8,69 @@ module tb (
 reg             clk;
 reg             rst;
 
-reg [31:0]      inst;
+reg[31:0] rom_mem[0:4095];
+
+wire [31:0]      inst;
 wire [31:0]     inst_addr;
+
+wire [31:0]x3 = tb.u_top.u_regs.regs[3];
+wire [31:0]x26= tb.u_top.u_regs.regs[26];
+wire [31:0]x27= tb.u_top.u_regs.regs[27];
+
+
+
+initial begin
+
+    $readmemh("E:/file/my-risc-v/sim/inst_txt/rv32ui-p-addi.txt",tb.u_rom.rom_mem);     //烧录指令
+
+    clk = 0;
+    rst = 1;
+    // inst = `INST_NOP;
+
+    #100
+    rst = 0;
+
+
+end
+
+integer r;
+	initial begin
+/* 		while(1)begin
+			@(posedge clk) 
+			$display("x27 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[27]);
+			$display("x28 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[28]);
+			$display("x29 register value is %d",tb.open_risc_v_soc_inst.open_risc_v_inst.regs_inst.regs[29]);
+			$display("---------------------------");
+			$display("---------------------------");
+		end */
+		wait(x26 == 32'b1);
+		
+		#200;
+		if(x27 == 32'b1) begin
+			$display("############################");
+			$display("########  pass  !!!#########");
+			$display("############################");
+		end
+		else begin
+			$display("############################");
+			$display("########  fail  !!!#########");
+			$display("############################");
+			$display("fail testnum = %2d", x3);
+            end
+	end
+
+always #5 clk = ~clk;
+
+// always @(*) begin
+//     case(inst_addr[3:2])
+//         2'b00: inst <= {12'd2, 5'd0, `INST_ADDI, 5'd10, `INST_TYPE_I};      //x10 = 2
+//         2'b01: inst <= {12'd1, 5'd11, `INST_ADDI, 5'd11, `INST_TYPE_I};     //x11 = x11 +1
+//         2'b10: inst <= {7'b0000000, `x10, `x11, `INST_ADD_SUB, `x12, `INST_TYPE_R_M};     //x12 = x11+x10
+//         2'b11: inst <= {7'b0100000, `x10, `x12, `INST_ADD_SUB, `x13, `INST_TYPE_R_M};   //x13 = x12 - x10
+        
+        
+//     endcase
+// end
 
 top u_top(
     .clk         (clk         ),
@@ -18,29 +79,11 @@ top u_top(
     .inst_addr_o (inst_addr )
 );
 
-initial begin
-    clk = 0;
-    rst = 1;
-    inst = `INST_NOP;
+rom u_rom(
+    .rom_addr_i (inst_addr ),
+    .rom_o      (inst      )
+);
 
-    #100
-    rst = 0;
-
-
-end
-
-always #5 clk = ~clk;
-
-always @(*) begin
-    case(inst_addr[3:2])
-        2'b00: inst <= {12'd2, 5'd0, `INST_ADDI, 5'd10, `INST_TYPE_I};      //x10 = 2
-        2'b01: inst <= {12'd1, 5'd11, `INST_ADDI, 5'd11, `INST_TYPE_I};     //x11 = x11 +1
-        2'b10: inst <= {7'b0000000, `x10, `x11, `INST_ADD_SUB, `x12, `INST_TYPE_R_M};     //x12 = x11+x10
-        2'b11: inst <= {7'b0100000, `x10, `x12, `INST_ADD_SUB, `x13, `INST_TYPE_R_M};   //x13 = x12 - x10
-        
-        
-    endcase
-end
 
     
 endmodule
