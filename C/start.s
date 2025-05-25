8bc870e4dbc31ac431ee91793b6f93423294993b
DawningSoon <dawningsoon@qq.com>
2025-05-25 15:13:40 +0800

    link stdint.h

--- C/start.s
+++ C/start.s
@@ -3,7 +3,7 @@
 
 _start:
     # 初始化堆栈指针 (假设堆栈地址为 0x80000000)
-    la sp, 0x2000  
+    la sp, 0x1000  
 
     # 跳转到 main 函数
     call main
--- C/test.c
+++ C/test.c
@@ -1,34 +1,48 @@
+#include <stdint-gcc.h>
 
+#define addr_base ((volatile uint32_t *)0x1000)
 
 int main()
 {
-    int i;
-    int *ptr = (int *)0x1000;
+    uint32_t i;
+    volatile uint32_t *ptr = addr_base;
     i=0;
     i= 'h';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'e';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'l';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'l';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'o';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= ' ';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'w';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'o';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'r';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'l';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= 'd';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     i= '!';
     *ptr = i;
+    ptr =(uint32_t*)(ptr +4);
     while(1)
     {
         // i++;
